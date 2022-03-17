368432190bd214bf44d76bfebc53e8c09d13b668
Jordan Kimsey <jordankimsey@gmail.com>
2022-03-17 11:05:48 -0400

    mobile layout complete

--- src/components/container.js
+++ src/components/container.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import patternDivider from './assets/images/pattern-divider-mobile.svg'
-import dice from './assets/images/icon-dice.svg'
+import patternDivider from './assets/images/pattern-divider-mobile.svg';
+import dice from './assets/images/icon-dice.svg';
 
 const container = () => {
   return (
-    <div className='container'>
-      <div className='adviceNumber'>Advice #117</div>
-      <div className='adviceQuote'>
-        "It is easy to sit up and take notice, what's difficult is getting up
-        and taking action."
-          </div>
-          <div className="lineBreak">
-              <div>-</div>
-              <img src={patternDivider} alt="pattern divider" />
-              <div>-</div>
-          </div>
-          <div className="diceButton">
-              <button><img src={dice} alt="" /></button>
-          </div>
-    </div>
+    <>
+      <div className='container'>
+        <div className='adviceNumber'>Advice #117</div>
+        <div className='adviceQuote'>
+          "It is easy to sit up and take notice, what's difficult is getting up
+          and taking action."
+        </div>
+        <div className='lineBreak'>
+          <hr></hr>
+          <img src={patternDivider} alt='pattern divider' />
+          <hr></hr>
+        </div>
+      </div>
+      <div className='diceButton'>
+        <button>
+          <img src={dice} alt='' />
+        </button>
+      </div>
+    </>
   );
 };
 
